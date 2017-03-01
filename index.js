5189f8928f83a67e5f47d51e88db1b1b2b10cc29
Javier Rodriguez <javier.rodleo@gmail.com>
2017-03-01 18:13:38 +0000

    make it better

---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = ( process.env.PORT || 3000);
+
+let base = '/api/v1';
+
+const contacts = [
+    {
+        "name": "Pepe",
+        "phone": "111222333",
+        "email": "pepe@pepe.com"
+    },
+    {
+        "name": "Antonio",
+        "phone": "444555666",
+        "email": "antonio@antonio.com"
+    }
+];
+
+// ROUTES
+
+app.get(base + '/contacts', (req, res) => {
+    res.send(contacts);
+});
+
+app.post(base + '/contacts', (req, res) => {
+   contacts.push(req.body);
+   res.send(req.body);
+   res.statusCode = 201;
+});
+
+
+app.use((request, response) => {
+    response.statusCode = 404;
+    response.end("404!");
+});
+
+
+app.listen(port, () => {
+    console.log('Server up and running');
+});
